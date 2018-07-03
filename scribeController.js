3cba1c7bae6d3a4044fc4a2436c431a8694dd502
wontonlegend <mileshong@students.berkeley.net>
2018-07-03 10:37:22 -0700

    The First commit

---
+++ scribeController.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var pg = require('pg');
+var path = require('path');
+var validate = require('jsonschema').validate;
+var moment = require('moment-timezone');
+
+var router = express.Router();
+
+//example json object array
+var json_ex = [
+
+];
+
+//schema to validate
+var schema = {
+    "id": "/simpleScribe",
+    "type": "object",
+    "properties": {
+        "scribe_id": {"type": "integer"},
+        "scribe_name": {"type": "string"},
+        "scribe_email": {"type": "string"},
+        "partner_name": {"type": "string"},
+        "login_time": {"type": "string"},
+        "browser": {"type": "string"},
+        "join_time": {"type": "string"},
+        "last_activity": {"type": "string"},
+        "user_type": {"type": "string"},
+        "doctor_name": {"type": "string"},
+        "doctor_email": {"type": "string"}
+    }
+};
+
+//insert json into database
+function insert_json(json) {
+    //get date
+    var local = 'America/Los_Angeles';
+    var d = moment(new Date());
+    d = d.tz(local).format('YYYY-MM-DD HH:mm:ss');
+
+    //connect to database
+    var pool = connect_db();
+
+    //insert the date and json into the table
+    pool.query("INSERT INTO scribes (ts, data) VALUES('" + d + "', '" + JSON.stringify(json) + "');", function (err, res) {
+        console.log(err, res);
+        pool.end();
+    });
+}
+
+//connect to postgres and axpractice db
+function connect_db() {
+    const pool = new pg.Pool({
+        user: 'postgres',
+        host: '127.0.0.1',
+        database: 'axpractice',
+        password: 'password',
+        port: '5432'
+    });
+
+    return pool;
+}
+
+//validate json and insert
+router.post('/add', function(req, res) {
+    var success = false;
+    for (var i = 0; i < json_ex.length; ++i) {
+        var validator = validate((json_ex[i]), schema);
+        if (validator.errors.length === 0) {
+            //insert json into postgres db
+            insert_json(json_ex[i]);
+            success = true;
+        }
+        else {
+            for (var err_msg_index = 0; i < validator.length; ++err_msg_index) {
+                console.log(validator[err_msg_index]);
+            }
+            success = false;
+        }
+    }
+    if (success) {
+        res.send("Successful Insert")
+    }
+
+});
+
+//get query results
+router.get('/get', function (req, res) {
+    var pool = connect_db();
+    var query = decodeURIComponent(req.query.query);
+
+    pool.query("SELECT data FROM scribes WHERE data->>" + query + ";", function(err, result) {
+        if (err) throw err;
+        res.json(result.rows);
+    })
+});
+
+//get main page html
+router.get('/', function(req, res) {
+   res.sendFile(path.join(__dirname, '../ax_practice_test/views', 'index.html'))
+});
+
+module.exports = router;
